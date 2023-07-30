92a3d7e24651cc0458d68f6bfbadf6a14c8292d8
iSlammedMyKindle <108752129+iSlammedMyKindle@users.noreply.github.com>
2023-07-30 00:31:20 -0400

    Notifications are done!

--- pages/toastnotification/index.mjs
+++ pages/toastnotification/index.mjs
@@ -1,5 +1,13 @@
+import toggleFade from "../common/toggleFade.mjs";
+
+window.wsListeners = ["macro"];
 window.jsonEvtHandlers = {
-    "macro":json=>console.log(json)
+    "macro":async json=>{
+        toast.children[0].innerText = json.user + " triggered: " + json.command;
+        notifySound.play();
+        await toggleFade(toast);
+        setTimeout(()=>toggleFade(toast, false), 5000);
+    }
 }
 
 // lazy import so stuff is in one file XP
